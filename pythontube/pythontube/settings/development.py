b7ffeb014cb8a01380a6df10d15c2a623616f461
ArsenalOkc <kaoruushjima@gmail.com>
2020-06-04 01:24:48 +0900

    update development settings: add django-debug-toolbar

--- pythontube/pythontube/settings/development.py
+++ pythontube/pythontube/settings/development.py
@@ -1,1 +1,10 @@
 from .partials import *
+
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
--- pythontube/pythontube/settings/partials/base.py
+++ pythontube/pythontube/settings/partials/base.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# BASE_DIR -> Project Folder가 있는곳
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)
+PROJECT_ROOT_DIR = os.path.dirname(BASE_DIR)  # .gitignore가 있는 곳
 
 
 # Quick-start development settings - unsuitable for production
