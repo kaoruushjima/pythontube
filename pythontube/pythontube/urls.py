367ff53c83922db06b20700d03ef1e4729ef68d4
ArsenalOkc <kaoruushjima@gmail.com>
2020-06-04 22:03:30 +0900

    init SignupView: render users/signup.html template

--- pythontube/pythontube/urls.py
+++ pythontube/pythontube/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+from users.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', SignupView.as_view(), name="signup"),
 ]
 
 if settings.DEBUG:
