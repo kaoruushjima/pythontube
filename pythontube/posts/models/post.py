d926c5d49b134a533c9b17d6237f2d6bdc6fb868
ArsenalOkc <kaoruushjima@gmail.com>
2020-06-11 00:04:28 +0900

    init Pos model: with youtube video id

---
+++ pythontube/posts/models/post.py
@@ -0,0 +1,26 @@
+from django.db import models
+from users.models import User
+
+
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.CharField(
+        max_length=16,
+    )
+    video_original_title = models.CharField(
+        max_length=256,
+        blank=True,
+        null=True,
+    )
+
+    title = models.CharField(
+        max_length=256,
+    )
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
