62fe96c63a6ee4516a8af51f228f2c61a7eadd31
ArsenalOkc <kaoruushjima@gmail.com>
2020-06-11 23:07:23 +0900

    refactor Post model: seperate youtube url utils

--- pythontube/posts/models/post.py
+++ pythontube/posts/models/post.py
@@ -23,15 +23,17 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def get_youtube_original_url(self):
-        return "https://www.youtube.com/watch?v={video_id}".format(
-                video_id=self.video_id,
-            )
+        from posts.utils.youtube import get_youtube_original_url as get_youtube_original_url_from_video_id
+        return get_youtube_original_url_from_video_id(
+            self.video_id
+        )
     # DATABASE는 변경되지 않음
     youtube_original_url = property(get_youtube_original_url)
 
     def get_youtube_embed_url(self):
-        return "https://www.youtube.com/embed/{video_id}".format(
-            video_id=self.video_id,
+        from posts.utils.youtube import get_youtube_embed_url as get_youtube_embed_url_from_video_id
+        return get_youtube_embed_url_from_video_id(
+            self.video_id
         )
     youtube_embed_url = property(get_youtube_embed_url)
 
