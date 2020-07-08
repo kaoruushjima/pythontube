a1bbc737484e01588460779981b04c188d196676
ArsenalOkc <kaoruushjima@gmail.com>
2020-07-08 23:01:23 +0900

    update PostCreateConfirmView: enable custom thumbnail image

--- pythontube/posts/models/post.py
+++ pythontube/posts/models/post.py
@@ -40,6 +40,13 @@ class Post(models.Model):
 
     youtube_thumbnail_image_url = property(get_youtube_thumbnail_image_url)
 
+    def get_thumbnail_image_url(self):
+        if self.thumbnail_image:
+            return self.thumbnail_image.url
+        return self.youtube_thumbnail_image_url
+
+    thumbnail_image_url = property(get_thumbnail_image_url)
+
     def get_youtube_original_url(self):
         from posts.utils.youtube import get_youtube_original_url as get_youtube_original_url_from_video_id
         return get_youtube_original_url_from_video_id(
