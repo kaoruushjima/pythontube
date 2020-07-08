d9ce18a784e34a33716cfac7ec6c679a7c472fda
ArsenalOkc <kaoruushjima@gmail.com>
2020-07-08 21:30:42 +0900

    init PostListView, PostDetailView

--- pythontube/posts/urls.py
+++ pythontube/posts/urls.py
@@ -5,7 +5,9 @@ from posts.views import *
 
 app_name = 'posts'
 urlpatterns = [
+    path('', PostListView.as_view(), name="list"),
     path('new/', PostCreateView.as_view(), name="create"),
     path('confirm/', PostCreateConfirmView.as_view(), name="confirm"),
+    path('<int:pk>/', PostDetailView.as_view(), name="detail"),
 
 ]
--- pythontube/posts/views/__init__.py
+++ pythontube/posts/views/__init__.py
@@ -1,1 +1,3 @@
 from .create import *
+from .list import PostListView
+from .detail import PostDetailView
