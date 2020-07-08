0d190e757d1cafbe391eb7e4534cc91c480ceab5
ArsenalOkc <kaoruushjima@gmail.com>
2020-07-08 21:55:35 +0900

    update Post model: implement get_absolute_url

--- pythontube/posts/models/post.py
+++ pythontube/posts/models/post.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from users.models import User
 
@@ -45,3 +46,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"pk": self.id})
