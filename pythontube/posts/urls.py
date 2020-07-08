a7c430f3e568b8100e5637ec0367177518bc6821
ArsenalOkc <kaoruushjima@gmail.com>
2020-07-08 22:04:31 +0900

    update PostDetailView: change pk to slug

--- pythontube/posts/urls.py
+++ pythontube/posts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name="list"),
     path('new/', PostCreateView.as_view(), name="create"),
     path('confirm/', PostCreateConfirmView.as_view(), name="confirm"),
-    path('<int:pk>/', PostDetailView.as_view(), name="detail"),
+    path('<slug:slug>/', PostDetailView.as_view(), name="detail"),
 
 ]
