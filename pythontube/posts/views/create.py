cf974af6c834ec04667b7634c41930d389410f90
ArsenalOkc <kaoruushjima@gmail.com>
2020-07-08 21:15:14 +0900

    update PostCreateView, PostCreateConfirmView: add LoginRequiredMixin

--- pythontube/posts/views/create.py
+++ pythontube/posts/views/create.py
@@ -1,11 +1,12 @@
 from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from posts.utils import youtube
 
 
-class PostCreateView(View):
+class PostCreateView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         return render(
@@ -28,7 +29,7 @@ class PostCreateView(View):
         return redirect(reverse("posts:create"))
 
 
-class PostCreateConfirmView(View):
+class PostCreateConfirmView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         return redirect(reverse("posts:create"))
